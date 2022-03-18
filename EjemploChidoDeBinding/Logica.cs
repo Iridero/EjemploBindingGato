38f2999688c6a4a261d04d3bfed03d5f2816a558
iridero <jcgarza_mx@hotmail.com>
2022-03-17 18:47:51 -0600

    Determinar ganador

--- EjemploChidoDeBinding/Logica.cs
+++ EjemploChidoDeBinding/Logica.cs
@@ -12,35 +12,73 @@ namespace EjemploChidoDeBinding
 {
     public class Logica : INotifyPropertyChanged
     {
-        private char[,] lineas;
+        private int[,] lineas;
         public ICommand JugarCommand { get; set; }
+        public ICommand IniciarCommand { get; set; }
         public char Turno { get; set; }
         public char[] Estado { get; set; }
+        public string Mensaje { get; set; }
+
 
         public Logica()
         {
-            Estado = new char[] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+            IniciarCommand = new RelayCommand(Iniciar);
             JugarCommand = new RelayCommand<string>(Jugar);
-            Turno= 'X';
+            
             // 0 1 2
             // 3 4 5
             // 6 7 8
-            lineas = new char[,] { { '0', '1', '2' }, { '3', '4', '5' }, { '6', '7', '8' }, 
-                                    { '0','3', '6' }, { '1', '4', '7' }, { '2', '5', '8' }, 
-                                    { '0', '4', '8' }, { '2', '4', '6' } };
+            lineas = new int[,] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, 
+                                    { 0,3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, 
+                                    { 0, 4, 8 }, { 2, 4, 6 } };
 
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        private bool ganó(char xo)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if (Estado[lineas[i,0]] == xo && 
+                    Estado[lineas[i, 1]] == xo && 
+                    Estado[lineas[i, 2]] == xo)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Iniciar()
+        {
+            Mensaje = String.Empty;
+            Estado = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+            Turno = 'X';
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
 
         public void Jugar(string valor)
         {
             int i = int.Parse(valor);
             Estado[i] = Turno;
             Turno = (Turno == 'X') ? 'O' : 'X';
+            if (ganó('X'))
+
+            {
+                Mensaje = "Ganó X";
+            }
+            if (ganó('O'))
+
+            {
+                Mensaje = "Ganó O";
+            }
+
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
             Trace.WriteLine($"Le diste clic al {valor}");
             Trace.WriteLine(new String(Estado));
+
+
         }
     }
 }
